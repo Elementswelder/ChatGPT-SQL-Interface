b36497ffb4244553c128727c45475205cb8f65df
Andrew W <andrewwwinsborrow@gmail.com>
2024-01-29 12:07:27 -0700

    Added the OPEN_AI connection

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 # This is a sample Python script.
+import os
+
 from openai import OpenAI
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+OPENAI_SECRET = os.environ.get('OPENAI_SECRET')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def start_program():
+    client = OpenAI(
+        api_key=OPENAI_SECRET
+    )
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "You are an SQL master and and you can only return your response in SQL questions."},
+            {"role": "user", "content": "How would I select a statement?"},
+        ]
+    )
+
+    print(completion)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    start_program()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
